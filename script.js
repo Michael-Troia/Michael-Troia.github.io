f8d0b4711350b5df157e72aaef3dd798e13bbd8c
Michael Troia <troia.michael.a@gmail.com>
2021-03-03 23:45:25 -0500

    feat: blog section added and first blog post

---
+++ script.js
@@ -0,0 +1,11 @@
+function showBlog() {
+    document.getElementById('Blog').style.display = "block";
+    document.getElementById('Portfolio').style.display = "none";
+    document.getElementById('Banner').style.display = "none";
+}
+
+function showPortfolio() {
+    document.getElementById('Blog').style.display = "none";
+    document.getElementById('Portfolio').style.display = "block";
+    document.getElementById('Banner').style.display = "block";
+}
